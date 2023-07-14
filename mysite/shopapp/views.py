d5c4dc99da01848b5ff35b14f2cde56dc8765909
Evgenii <evgeniikatunkin@gmail.com>
2023-07-14 09:25:30 +0200

    Create base and index pages in template/shopapp. Create view and urls for index page in shopapp.

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+from timeit import default_timer
 
-# Create your views here.
+
+def index(request: HttpRequest):
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 499),
+    ]
+    context = {
+        'time_running': default_timer(),
+        'products': products,
+    }
+    return render(request, 'shopapp/index.html', context=context)
